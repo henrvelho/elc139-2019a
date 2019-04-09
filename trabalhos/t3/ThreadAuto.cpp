6debdbbe0e5e45c06a18878ec28e35129dd9997e
henrvelho <henrvelho@gmail.com>
2019-04-09 01:58:34 -0300

    t3

---
+++ trabalhos/t3/ThreadAuto.cpp
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <omp.h>
+//#define chunk 2
+//int chunk;
+
+class SharedArray
+{
+    private:
+        char *array;
+        int index;
+        int size;
+
+    public:
+        SharedArray(int n) : size(n), index(0)
+        {
+            array = new char[size];
+            std::fill(array, array + size, '-');
+        }
+
+        ~SharedArray()
+        {
+            delete[] array;
+        }
+
+        void VaiAlahVonte(char c)
+        {
+            array[index] = c;
+            spendSomeTime();
+            index++;
+        }
+
+        int countOccurrences(char c)
+        {
+            return std::count(array, array + size, c);
+        }
+
+        std::string toString()
+        {
+            return std::string(array, size);
+        }
+
+    private:
+        void spendSomeTime()
+        {
+            for (int i = 0; i < 10000; i++)
+                for (int j = 0; j < 1000; j++);
+        }
+};
+
+class ArrayFiller
+{
+    private:
+        static const int nThreads = 3;
+        static const int nTimes = 20;
+        SharedArray *array;
+
+    public:
+        ArrayFiller()
+        {
+            array = new SharedArray(nThreads * nTimes);
+        }
+
+        void fillArrayConcurrently(int op)
+        {   
+            int i;
+            
+                
+                    #pragma omp parallel for schedule(auto) num_threads(nThreads) private(i)
+                    for (i = 0; i < nTimes*nThreads; i++)
+                        array->VaiAlahVonte('A' + omp_get_thread_num());
+                    
+            
+        }
+
+        void printStats()
+        {
+            std::cout << array->toString() << std::endl;
+            for (int i = 0; i < nThreads; ++i)
+                std::cout << (char)('A' + i) << "="
+                        << array->countOccurrences('A' + i) << " ";
+            std::cout << std::endl;
+        }
+
+        ~ArrayFiller()
+        {
+            delete array;
+        }
+};
+
+int main()
+{
+    std::cout << "Each thread should add its char to the array n times (n=20)" << std::endl;
+    std::cout << "Correct results should total exactly nThreads*nTimes chars" << std::endl;
+
+    std::cout << "\nSem tratamento de mutex e  scheduling AUTO (resultado errado)" << std::endl;
+    ArrayFiller c1;
+    c1.fillArrayConcurrently(0);
+    c1.printStats();
+
+}
