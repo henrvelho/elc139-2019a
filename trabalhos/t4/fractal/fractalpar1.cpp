0e9c45b782bd547a18d53f7c23ec416c7399e05f
henrvelho <henrvelho@gmail.com>
2019-04-21 21:43:52 -0300

    Update fractalpar1.cpp

--- trabalhos/t4/fractal/fractalpar1.cpp
+++ trabalhos/t4/fractal/fractalpar1.cpp
@@ -35,8 +35,8 @@ int main(int argc, char *argv[])
     const double xMin = xMid - delta;   
     const double yMin = yMid - delta;
     const double dw = 2.0 * delta / width;
-    //#pragma omp parallel private(col, frame, row) 
-    #pragma omp parallel for schedule(dynamic) num_threads(16)
+    #pragma omp parallel private(col, frame, row) 
+    #pragma omp parallel for schedule(dynamic) num_threads(4)
     
     for (row = 0; row < width; row++) {
     	
